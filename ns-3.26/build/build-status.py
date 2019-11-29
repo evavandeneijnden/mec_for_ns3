25f122496cf07525c63eca96858a8a4c54f861a2
Eva van den Eijnden <eva.vandeneijnden@tno.nl>
2019-11-29 15:23:44 +0000

    removed ignore build files

--- ns-3.26/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.26-lte-mec-debug', 'build/scratch/ns3.26-scratch-simulator-debug', 'build/scratch/subdir/ns3.26-subdir-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
